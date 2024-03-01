ee713c4d035d4f135a13ae1428645fea7380d11b
joelshaji5 <joel.shaji15@gmail.com>
2024-03-01 19:46:42 +0530

    homepage check

--- src/app/Components/Components/Footer/Footer.jsx
+++ src/app/Components/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../../../Style/Footer.css"
+import Image from 'next/image'
 
 function Footer() {
   return (
@@ -10,9 +11,29 @@ function Footer() {
     <img src='./footerLogo.png' alt='logo' className='FooterLogo' />
 
     </div>
-    <div className='footerTxtDiv'>
-        <div></div>
-        <div>
+    <div className='footerTxtDiv flex-col'>
+        <div className='flex SocialMediaLogoDiv'>
+            <div>  <Image
+      src="/FbLogo.png"
+      width={24}
+      height={24}
+      alt="Facebook"
+    /></div>
+            <div className='ml-3'>  <Image
+      src="/InstaLogo.png"
+      width={24}
+      height={24}
+      alt="Instagram"
+    /></div>
+            <div className='ml-3'>  <Image
+      src="/TweeterLogo.png"
+      width={24}
+      height={24}
+      alt="Tweeter"
+    /></div>
+
+        </div>
+        <div className='FooterDivH3'> 
         <h3>Location - Pune, Navi Mumbai</h3>
         <h3>Email ID - homnident@gmail.com</h3>
         <h3>Phone number - 95037 75068</h3>
